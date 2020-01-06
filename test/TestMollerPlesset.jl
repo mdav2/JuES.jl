e2bfc3e9b47a3567b3e40ffd08ddcc255d90d903
matt davis <matthew.davis010@gmail.com>
2020-01-05 20:15:10 -0500

    changed to pk

--- test/TestMollerPlesset.jl
+++ test/TestMollerPlesset.jl
@@ -7,7 +7,8 @@ psi4 = pyimport("psi4")
 psi4.core.be_quiet() #turn off output
 # > setup
 tol = 1E-14
-psi4.set_options(Dict("D_CONVERGENCE" => 10))
+psi4.set_options(Dict("D_CONVERGENCE" => 10,
+					  "scf_type" => "pk"))
 mol2 = psi4.geometry("""
 					 O
 					 H 1 1.1
@@ -27,7 +28,7 @@ psi4.set_options(Dict("reference" => "uhf"))
 e3,wfn3 = psi4.energy("hf/sto-3g",mol=mol3,return_wfn=true)
 JuWfn3 = Wfn(wfn3,Float64,true,false)
 @testset "MP2" begin
-	@test do_rmp2(JuWfn2) ≈ -0.04915088620016106
+	@test do_rmp2(JuWfn2) ≈ -0.04914964480386458
 	@test do_ump2(JuWfn3) ≈ -0.03588729625230
 	@test do_rmp2(JuWfn2) ≈ do_ump2(JuWfn2)
 end
