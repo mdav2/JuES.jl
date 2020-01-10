e8e25993c45d9ec51806478da066f8bb27d3b4df
matt davis <matthew.davis010@gmail.com>
2020-01-09 20:22:36 -0500

    update TestCISingles to catch bug in CISingles

--- test/TestCISingles.jl
+++ test/TestCISingles.jl
@@ -6,10 +6,11 @@ psi4 = pyimport("psi4")
 psi4.core.be_quiet() #turn off output
 tol = 1E-14
 mol = psi4.geometry("""
-					H
-					H 1 1.0
-					symmetry c1
-					""")
+                    O
+                    H 1 1.1
+                    H 1 1.1 2 104.0
+		    symmetry c1
+		    """)
 psi4.set_options(Dict("scf_type" => "pk","d_convergence"=>14))
 e,wfn = psi4.energy("hf/sto-3g",mol=mol,return_wfn=true)
 JuWfn = Wfn(wfn)
